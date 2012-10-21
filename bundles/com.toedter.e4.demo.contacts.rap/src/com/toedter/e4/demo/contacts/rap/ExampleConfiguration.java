21e8e86100d70c3ae91a0faf32eb4b26a306e76f
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-10-21 12:04:41 +0200

    Register entrypoint without plugin.xml
    Neither a plugin.xml nor a product is needed to start a RAP application.
    The necessary e4 branding properties are provided to the
    E4EntryPointFactory.

--- bundles/com.toedter.e4.demo.contacts.rap/src/com/toedter/e4/demo/contacts/rap/ExampleConfiguration.java
+++ bundles/com.toedter.e4.demo.contacts.rap/src/com/toedter/e4/demo/contacts/rap/ExampleConfiguration.java
@@ -1,5 +1,7 @@
 package com.toedter.e4.demo.contacts.rap;
 
+import org.eclipse.e4.ui.internal.workbench.E4Workbench;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,15 +12,17 @@ import org.eclipse.rap.rwt.application.ApplicationConfiguration;
 import org.eclipse.rap.rwt.client.WebClient;
 import org.eclipse.rap.rwt.lifecycle.IEntryPointFactory;
 
+
 public class ExampleConfiguration implements ApplicationConfiguration {
 
-	@Override
-	public void configure(Application application) {
-		Map<String, String> properties = new HashMap<String, String>();
-		properties.put(WebClient.PAGE_TITLE, "Contacts on RAP");
-		IEntryPointFactory entryPointFactory = new E4EntryPointFactory("E4 RAP Contacs Demo");
-		application.addEntryPoint("/contacts", entryPointFactory, properties);
-		application.setOperationMode( OperationMode.SWT_COMPATIBILITY);
-	}
+  @Override
+  public void configure( Application application ) {
+    Map<String, String> properties = new HashMap<String, String>();
+    properties.put( WebClient.PAGE_TITLE, "Contacts on RAP" );
+    properties.put( E4Workbench.XMI_URI_ARG, "com.toedter.e4.demo.contacts.generic/Application.e4xmi" );
+    IEntryPointFactory entryPointFactory = new E4EntryPointFactory( properties );
+    application.addEntryPoint( "/contacts", entryPointFactory, properties );
+    application.setOperationMode( OperationMode.SWT_COMPATIBILITY );
+  }
 
 }
--- bundles/example.e4.app/src/example/e4/app/ExampleConfiguration.java
+++ bundles/example.e4.app/src/example/e4/app/ExampleConfiguration.java
@@ -3,22 +3,24 @@ package example.e4.app;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.e4.ui.internal.workbench.E4Workbench;
 import org.eclipse.e4.ui.workbench.rap.E4EntryPointFactory;
 import org.eclipse.rap.rwt.application.Application;
 import org.eclipse.rap.rwt.application.Application.OperationMode;
 import org.eclipse.rap.rwt.application.ApplicationConfiguration;
 import org.eclipse.rap.rwt.client.WebClient;
-import org.eclipse.rap.rwt.lifecycle.IEntryPointFactory;
+
 
 public class ExampleConfiguration implements ApplicationConfiguration {
 
-	@Override
-	public void configure(Application application) {
-		Map<String, String> properties = new HashMap<String, String>();
-		properties.put(WebClient.PAGE_TITLE, "E4 on RAP");
-		IEntryPointFactory entryPointFactory = new E4EntryPointFactory("example.e4.app");
-		application.addEntryPoint("/e4example", entryPointFactory, properties);
-		application.setOperationMode( OperationMode.SWT_COMPATIBILITY);
-	}
+  @Override
+  public void configure( Application application ) {
+    Map<String, String> properties = new HashMap<String, String>();
+    properties.put( WebClient.PAGE_TITLE, "E4 on RAP" );
+    properties.put( E4Workbench.XMI_URI_ARG, "example.e4.app/Application.e4xmi" );
+    properties.put( E4Workbench.CSS_URI_ARG, "platform:/plugin/example.e4.app/css/default.css" );
+    application.addEntryPoint( "/e4example", new E4EntryPointFactory( properties ), properties );
+    application.setOperationMode( OperationMode.SWT_COMPATIBILITY );
+  }
 
 }
